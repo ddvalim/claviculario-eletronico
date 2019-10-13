c1c916a700804a8618204a54046bc4516e1ca383
Diovana Rodrigues Valim <53877301+ddvalim@users.noreply.github.com>
2019-10-13 17:49:00 -0300

    Update ControlePrincipal.py

--- ControlePrincipal.py
+++ ControlePrincipal.py
@@ -1,6 +1,8 @@
 from Veiculo.ControleVeiculo import ControleVeiculo
 from Funcionario.ControleFuncionarios import ControleFuncionarios
 from MovimentoVeiculo.ControleMovimentacao import ControleMovimentacao
+from entradaInvalidaException import EntradaInvalidaException
+from EOFError import EOFErrorException
 
 class ControlePrincipal:
 
@@ -21,75 +23,82 @@ class ControlePrincipal:
                 print('1 - Gerenciar veículos')
                 print('2 - Gerenciar movimentações')
 
-                opcao_selecionada = input()
+                opcao = input()
 
-                if opcao_selecionada == 0:
-                    print('0 - Adicionar funcionário')
-                    print('1 - Remover funcionário')
-                    print('2 - Autenticar funcionário')
-                    print('3 - Atualizar funcionário')
-                    print('4 - Obter funcionários cadastrados')
-                    print('5 - Adicionar veículo à funcionário')
-                    print('6 - Remover veículo de funcionário')
-                    print('7 - Bloquear funcionário')
-                    print('8 - Adicionar funcionário')
+                if opcao != 0 and opcao != 1 and opcao != 2:
+                    raise EntradaInvalidaException()
+                else:
+                    if opcao_selecionada == 0:
+                        print('0 - Adicionar funcionário')
+                        print('1 - Remover funcionário')
+                        print('2 - Autenticar funcionário')
+                        print('3 - Atualizar funcionário')
+                        print('4 - Obter funcionários cadastrados')
+                        print('5 - Adicionar veículo à funcionário')
+                        print('6 - Remover veículo de funcionário')
+                        print('7 - Bloquear funcionário')
+                        print('8 - Adicionar funcionário')
 
-                    reopcao = input()
+                        reopcao = input()
 
-                    if reopcao == 0:
-                        adiciona_funcionario()
-                    elif reopcao == 1:
-                        deleta_funcionario()
-                    elif reopcao == 2:
-                        autentica_funcionario()
-                    elif reopcao == 3:
-                        atualiza_funcionario()
-                    elif reopcao == 4:
-                        funcionarios_cadastrados()
-                    elif reopcao == 5:
-                        adiciona_veiculo_funcionario()
-                    elif reopcao == 6:
-                        deleta_veiculo_funcionario()
-                    elif reopcao == 7:
-                        bloqueia_funcionario()
-                    elif reopcao == 8:
-                        adiciona_funcionario()
-                    else:
-                        print("Comando inválido.")
+                        if reopcao == 0:
+                            adiciona_funcionario()
+                        elif reopcao == 1:
+                            deleta_funcionario()
+                        elif reopcao == 2:
+                            autentica_funcionario()
+                        elif reopcao == 3:
+                            atualiza_funcionario()
+                        elif reopcao == 4:
+                            funcionarios_cadastrados()
+                        elif reopcao == 5:
+                            adiciona_veiculo_funcionario()
+                        elif reopcao == 6:
+                            deleta_veiculo_funcionario()
+                        elif reopcao == 7:
+                            bloqueia_funcionario()
+                        elif reopcao == 8:
+                            adiciona_funcionario()
+                        else:
+                            raise EntradaInvalidaException()
 
-                elif opcao_selecionada == 1:
-                    print('0 - Verificar veículo')
-                    print('1 - Listar veículos cadastrados')
-                    print('2 - Adicionar veículo')
-                    print('3 - Remover veículo')
-                    print('4 - Atualizar quilometragem de um veículo')
-                    print('5 - Atualizar veículo')
+                    elif opcao_selecionada == 1:
+                        print('0 - Verificar veículo')
+                        print('1 - Listar veículos cadastrados')
+                        print('2 - Adicionar veículo')
+                        print('3 - Remover veículo')
+                        print('4 - Atualizar quilometragem de um veículo')
+                        print('5 - Atualizar veículo')
 
-                    reopcao = input()
+                        reopcao = input()
 
-                    if reopcao == 0:
-                        verifica_veiculo()
-                    elif reopcao == 1:
-                        veiculos_cadastrados()
-                    elif reopcao == 2:
-                        adiciona_veiculo()
-                    elif reopcao == 3:
-                        deleta_veiculo()
-                    elif reopcao == 4:
-                        atualiza_km_veiculo()
-                    elif reopcao == 5:
-                        atualiza_veiculo()
-                    else:
-                        print("Comando inválido.")
+                        if reopcao == 0:
+                            verifica_veiculo()
+                        elif reopcao == 1:
+                            veiculos_cadastrados()
+                        elif reopcao == 2:
+                            adiciona_veiculo()
+                        elif reopcao == 3:
+                            deleta_veiculo()
+                        elif reopcao == 4:
+                            atualiza_km_veiculo()
+                        elif reopcao == 5:
+                            atualiza_veiculo()
+                        else:
+                            raise EntradaInvalidaException()
 
-                elif opcao_selecionada == 2:
-                    print('0 - Filtrar movimentação')
-                    print('1 - Obter relatório por tipo')
-                    print('2 - Retirar veículo')
-                    print('3 - Devolver veículo')
-                    
-                    reopcao = input()
-                    
-                    if reopcao == 0:
-                        
+                    elif opcao_selecionada == 2:
+                        print('0 - Filtrar movimentação')
+                        print('1 - Obter relatório por tipo')
+                        print('2 - Retirar veículo')
+                        print('3 - Devolver veículo')
+
+                        reopcao = input()
+
+                        if reopcao == 0:
+            
+            
+            
+            except EOFError:
+                raise EOFErrorException()
 
