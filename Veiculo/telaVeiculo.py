13ec2055d30ac0a334c89784fb9602620581394e
Diovana Rodrigues Valim <53877301+ddvalim@users.noreply.github.com>
2019-10-13 10:16:10 -0300

    Update telaVeiculo.py

--- Veiculo/telaVeiculo.py
+++ Veiculo/telaVeiculo.py
@@ -1,4 +1,47 @@
 from Abstracts.abs_tela import AbstractTela
+from ControleVeiculo import ControleVeiculo
 
 class telaVeiculo(AbstractTela):
-    def __init__(self,):
+    def __init__(self):
+        pass
+
+    def lista_veiculos(self):
+        print('*' * 30)
+        print('Listando veículos...')
+        print('*' * 30)
+
+        for veiculo in veiculos_cadastrados:
+            print(veiculo)
+
+    def detalhes_veiculo(self, placa):
+        if placa in veiculos_cadastrados.keys():
+            vel = veiculos_cadastrados[placa]
+            print(vel.modelo)
+            print(vel.marca)
+            print(vel.ano)
+            print(vel.km)
+
+    def adiciona_veiculo(self, veiculo:Veiculo):
+        mensagem = 'Ocorreu um erro durante o cadastramento. Tente novamente.'
+        print('*' * 30)
+        print('Adicionando veículo...')
+        print('*' * 30)
+
+        print('Veículo cadastrado: ' + veiculo) if adiciona_veiculo(veiculo) == True else print(mensagem)
+
+    def remove_veiculo(self, veiculo:Veiculo):
+        mensagem = 'Ocorreu um erro durante o cadastramento. Tente novamente.'
+        print("*" * 30)
+        print('Removendo veículo...')
+        print('*' * 30)
+
+        print('Veículo removido: ' + veiculo) if deleta_veiculo(veiculo) == True else print(mensagem)
+
+    def atualiza_veiculo(self, veiculo:Veiculo):
+        mensagem = 'Ocorreu um erro durante o cadastramento. Tente novamente.'
+        print('*' * 30)
+        print('Atualizando veículo...')
+        print('*' * 30)
+
+        print('Veículo atualizado: ' + veiculo) if atualiza_veiculo(veiculo) == True else print(mensagem)
+
