fb8e47f659e228a396f3ed15b116b0d98bc8981d
Gabriel Aristeu Cabral <gabriel.aristeu.cabral@gmail.com>
2019-11-21 20:03:49 -0200

    add close in exception

--- Abstracts/AbsctractTela.py
+++ Abstracts/AbsctractTela.py
@@ -9,6 +9,7 @@ class AbstractTela(ABC):
     def show(self):
         button, value = self.window.Read()
         if button is None:
+            self.close()
             raise VoltarMenuException()
         return button, value
 
